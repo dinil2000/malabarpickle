adcb354c441f6a63183cc28956162077cc5ba3aa
Dinil Das <dinildasc@gmail.com>
2026-08-12 23:00:28 +0530

    Configure Google OAuth Client ID 979669399574-5khaf0tuebnfrr8ba7r4q2hbr7l500hf for live Account Chooser popup

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"lle5IgkXL+KjMww8QD7hqvBWvlmSaqPxmffs15xC7eE=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"V2zH109J4EeYK3GayEG1KQEwtfH2QdiRnwFWu88ks8c=\"}"
